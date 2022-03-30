f057d38e9ba39ca820efd31e669e66cc62a4873e
lsa <pingu944@gmail.com>
2022-03-30 17:11:40 +0900

    Modify : haeder ,nav depth1 complete

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,40 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Header } from './Header';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-import { Header } from './Header.js';
-import { SearchBar } from './SearchBar.js';
+/*import { SearchBar } from './SearchBar.js';*/
 import './Nav.scss';
 
 const Nav = () => {
   return (
-    <div className="wrap">
-      <Header />
-      <nav className="nav">
-        <SearchBar />
-      </nav>
-    </div>
+    <nav className="nav">
+      <div className="wrap">
+        <Header />
+        <ul class="nav-depth1">
+          <li className="nav-category">
+            <FontAwesomeIcon icon={faBars} className="menu-bar" />
+            <span>제품카테고리</span>
+          </li>
+          <li>
+            <Link to="#">신상품</Link>
+          </li>
+          <li>
+            <Link to="#">베스트</Link>
+          </li>
+          <li>
+            <Link to="#">봄,들이기</Link>
+          </li>
+          <li>
+            <Link to="#">데코뷰 스타일링</Link>
+          </li>
+          <li>
+            <Link to="#">기획전</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
