2972f872806d2beff461065d89558c050ae5ceb6
lsa <pingu944@gmail.com>
2022-04-04 10:44:30 +0900

    Modify : index.html modified

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
